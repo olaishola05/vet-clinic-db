bb4d2f29e4d0094d20c83fae7c25f627eda293fd
Ola Ishola <olaishola@hotmail.co.uk>
2022-03-21 10:14:12 +0100

    Add first data into the animals data && format the date

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
-/* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
+
+INSERT INTO animals (name, date_of_birth, neutered, weight_kg) VALUES ('Agumon', '2020-02-03', true, 10.23);
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
