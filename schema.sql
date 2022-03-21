532bb3cd33b3696478366ce8d6474c888dfcd624
Ola Ishola <olaishola@hotmail.co.uk>
2022-03-21 10:33:29 +0100

    Insert datas into Animal Table and add .gitignore file

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY,
     name varchar(100) NOT NULL,
-    date_of_birth date NOT NULL,
+    date_of_birth DATE NOT NULL,
     escape_attempts int NOT NULL DEFAULT 0,
     neutered boolean NOT NULL DEFAULT false,
     weight_kg float NOT NULL DEFAULT 0,
