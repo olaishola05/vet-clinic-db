871212be87028bdd37b4de051bd9db93ba86da05
Ola Ishola <olaishola@hotmail.co.uk>
2022-03-24 13:30:39 +0100

    Add queries for join

--- queries.sql
+++ queries.sql
@@ -58,3 +58,26 @@ SELECT species,
     SUM(escape_attempts),
     COUNT(escape_attempts),
     SUM(escape_attempts) / COUNT(escape_attempts) as Average FROM animals WHERE extract(YEAR FROM date_of_birth) BETWEEN 1990 AND 2000 GROUP BY species;
+
+-- Joining Tables
+
+SELECT AN.name, date_of_birth, escape_attempts, 
+neutered,weight_kg, species_id, O.owners_id AS "OWNERS ID", full_name FROM animals AN 
+JOIN owners O ON O.owners_id = AN.owners_id WHERE O.full_name = 'Melody Pond'
+
+SELECT * FROM animals AS AN 
+JOIN species S ON S.species_id = AN.species_id
+WHERE S.name = 'Pokemon'
+
+SELECT * FROM owners O LEFT JOIN animals AN ON AN.owners_id = O.owners_id
+SELECT S.name, COUNT(AN.species_id) FROM animals AN 
+LEFT JOIN species S on S.species_id = AN.species_id GROUP BY S.name;
+
+SELECT * FROM animals AN JOIN owners O ON O.owners_id = AN.owners_id 
+WHERE O.full_name = 'Jennifer Orwell';
+
+SELECT * FROM animals AN JOIN owners O ON O.owners_id = AN.owners_id 
+WHERE O.full_name = 'Dean Winchester' AND AN.escape_attempts <= 0;
+
+SELECT O.full_name, COUNT(O.owners_id) FROM animals AN 
+LEFT JOIN owners O ON O.owners_id = AN.owners_id GROUP BY O.full_name ORDER BY COUNT(*) DESC;
